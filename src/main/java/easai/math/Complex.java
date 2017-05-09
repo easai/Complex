4dddb8a82bea22596ea705f60addff43fdd54887
easai <aksarih@gmail.com>
2017-05-09 05:41:39 -0400

    Added Main-Class attribute using maven-jar-plugin.

--- src/main/java/easai/math/Complex.java
+++ src/main/java/easai/math/Complex.java
@@ -7,23 +7,6 @@
 //Created: Wed Dec 18 06:17:27 2013
 //Keywords: math, mathematics, complex numbers, imaginary numbers, complex analysis
 
-//This file is not part of GNU Emacs.
-
-//Complex.java is free software; you can redistribute it and/or modify
-//it under the terms of the GNU General Public License as published by
-//the Free Software Foundation; either version 2, or (at your option)
-//any later version.
-
-//This software is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
-
-//You should have received a copy of the GNU General Public License
-//along with this program; see the file COPYING.  If not, write to the
-//Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-//Boston, MA 02111-1307, USA.
-
 //Commentary:
 //This is an implementation of a complex number class.
 //
